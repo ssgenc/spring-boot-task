153783c4a455db9066f504d1c95b62e8454f7ef6
sirhot <serhatgenc@msn.com>
2020-10-08 00:25:09 +0300

    User Controller created. getAllUsers, getUser by id, create user, delete user by id functions added.

---
+++ spring-task/src/main/java/com/accenture/springtask/controller/UserResourceController.java
@@ -0,0 +1,58 @@
+package com.accenture.springtask.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.accenture.springtask.model.User;
+import com.accenture.springtask.service.UserDaoService;
+
+@RestController
+public class UserResourceController {
+
+	private UserDaoService service;
+
+	public UserResourceController(UserDaoService service) {
+		super();
+		this.service = service;
+	}
+
+	@GetMapping("/users")
+	public List<User> getAllUsers() {
+		return service.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public User getUser(@PathVariable int id) {
+		User user = service.findUserById(id);
+		return user;
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
+
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUserById(@PathVariable int id) {
+		service.deleteUserById(id);
+		
+	}
+
+}
