ac674f4c5a2416871cf3081114582d62337ddca5
sirhot <serhatgenc@msn.com>
2020-10-07 23:46:23 +0300

    Spring Boot project created.

---
+++ spring-task/src/test/java/com/accenture/springtask/SpringTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.springtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
